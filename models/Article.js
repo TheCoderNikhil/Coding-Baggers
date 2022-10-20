ce4a36d4a704cbe94b2949ef0615ede1e9af1f01
nikhil garg <nikhilgargcoder@gmail.com>
2022-10-20 10:17:42 +0530

    Update Article.js

--- models/Article.js
+++ models/Article.js
@@ -1,55 +1,57 @@
-const mongoose = require('mongoose');
-const slugify = require('slugify')
-const marked = require('marked');
-const createDOMPurify = require('dompurify');
-const { JSDOM } = require('jsdom');
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const marked = require("marked");
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
 
-const window = new JSDOM('').window;
+const window = new JSDOM("").window;
 const DOMPurify = createDOMPurify(window);
 
 const articleSchema = mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   description: {
     type: String,
-    required: true
+    required: true,
   },
   markdown: {
     type: String,
-    required: true
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: () => Date.now()
+    default: () => Date.now(),
   },
   slug: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   sanitizedHtml: {
     type: String,
-    required: true
+    required: true,
   },
-  img:
-  {
-      data: Buffer,
-      contentType: String
-  }
-})
+  required: {
+    type: Boolean,
+  },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+});
 
-articleSchema.pre('validate', function (next) {
+articleSchema.pre("validate", function (next) {
   if (this.title) {
-    this.slug = slugify(this.title, { lower: true, strict: true })
+    this.slug = slugify(this.title, { lower: true, strict: true });
   }
   if (this.markdown) {
-    this.sanitizedHtml = DOMPurify.sanitize(marked(this.markdown))
+    this.sanitizedHtml = DOMPurify.sanitize(marked(this.markdown));
   }
-  next()
-})
+  next();
+});
 
-const Article = mongoose.model('Article', articleSchema)
+const Article = mongoose.model("Article", articleSchema);
 
-module.exports = Article
+module.exports = Article;
